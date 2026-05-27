2736471303400f3a8228c8a3d9b4094120a220cc
Jonathan Guichard <jona.guich69@gmail.com>
2026-05-28 01:40:59 +0200

    Back -- Fix local date and ComputeOrderDate behaviour

--- back/Abstractions/Helpers/OrderDateHelper.cs
+++ back/Abstractions/Helpers/OrderDateHelper.cs
@@ -2,7 +2,7 @@ namespace SousMarinJaune.Api.Abstractions.Helpers;
 
 public static class OrderDateHelper
 {
-	public static readonly TimeSpan Cutoff = new(11, 30, 0);
+	private static readonly TimeSpan Cutoff = new(11, 30, 0);
 
 	// La cuisine n'ouvre que le jeudi. On ne peut commander pour aujourd'hui
 	// qu'un jeudi avant 11h30 ; sinon la commande est reportée (et l'utilisateur prévenu).
@@ -10,10 +10,13 @@ public static class OrderDateHelper
 
 	public static DateTime ComputeOrderDate(DateTime now)
 	{
-		if (!IsDeferred(now)) return now;
+		if (!IsDeferred(now))
+		{
+			return new DateTime(now.Year, now.Month, now.Day, 11, 30, 0, DateTimeKind.Local);
+		}
 
 		// Jeudi après 11h30 : on saute au jeudi suivant ; sinon on cible le prochain jeudi.
-		var daysUntilThursday = ((int) DayOfWeek.Thursday - (int) now.DayOfWeek + 7) % 7;
+		var daysUntilThursday = ((int)DayOfWeek.Thursday - (int)now.DayOfWeek + 7) % 7;
 		if (daysUntilThursday == 0) daysUntilThursday = 7;
 		return now.Date.AddDays(daysUntilThursday).AddHours(12);
 	}
